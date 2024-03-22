2ecdce7c8dcfc45ee85f4b033f0105705f1e9849
Ruth <ruthellennds@gmail.com>
2024-03-21 23:08:03 -0300

    first commit

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementService.kt
@@ -0,0 +1,42 @@
+package br.edu.ifsp.scl.sdm.entityservicecommunication
+
+import android.app.Service
+import android.content.Intent
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.IBinder
+import android.os.Looper
+import android.os.Message
+
+class IncrementService : Service() {
+
+    private inner class IncrementHandler(looper: Looper): Handler(looper) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            msg.data.getInt("VALUE").also {
+                InterEntityCommunication.valueLiveData.postValue(it + 1)
+                Intent("INCREMENT_VALUE_ACTION").putExtra("VALUE", it + 1).apply {
+                    sendBroadcast(this)
+                }
+            }
+            stopSelf()
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent?.getIntExtra("VALUE", -1)?.also { value ->
+            HandlerThread("IncrementThread").apply {
+                start()
+                IncrementHandler(looper).apply {
+                    obtainMessage().apply {
+                        data.putInt("VALUE", value)
+                        sendMessage(this)
+                    }
+                }
+            }
+        }
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? = null
+}
---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
@@ -0,0 +1,65 @@
+package br.edu.ifsp.scl.sdm.entityservicecommunication
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.sdm.entityservicecommunication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var  incrementServiceIntent: Intent
+    private var counter = 0
+
+    private val incrementBroadcastReceiver = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            intent?.getIntExtra("VALUE", -1)?.also {
+                counter = it
+                Toast.makeText(this@MainActivity, "You clicked $counter times.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+        incrementServiceIntent = Intent(this, IncrementService::class.java)
+
+        with (amb) {
+            mainTb.apply {
+                getString(R.string.app_name).also { setTitle(it) }
+                setSupportActionBar(this)
+            }
+            incrementBt.setOnClickListener {
+                startService(incrementServiceIntent.apply {
+                    putExtra("VALUE", counter)
+                })
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(incrementBroadcastReceiver, IntentFilter("INCREMENT_VALUE_ACTION"),
+            RECEIVER_EXPORTED)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(incrementBroadcastReceiver)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(incrementServiceIntent)
+    }
+}
